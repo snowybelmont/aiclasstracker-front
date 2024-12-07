7ed27754035333a757ad80fd061884b1dce6b170
snowybelmont <adson.vitorio@fatec.sp.gov.br>
2024-12-06 22:11:18 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
+import StackNavigator from '@/routes/Stack'
+import { loginService } from '@/services/LoginService'
+
+export default function App() {
+    loginService.defineInterceptor()
+
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+          <NavigationContainer>
+              <StackNavigator />
+              <StatusBar style='light' backgroundColor='#663399' />
+          </NavigationContainer>
+      </SafeAreaView>
+    )
+}
