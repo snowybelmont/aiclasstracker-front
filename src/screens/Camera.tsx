334b8cd8defeaef99e23eb7b150b7ad80eedc22c
snowybelmont <adson.vitorio@fatec.sp.gov.br>
2024-12-10 13:46:00 -0300

    Initial commit

---
+++ src/screens/Camera.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Camera = () => {
+    return (
+        <View>
+            <Text>Camera</Text>
+        </View>
+    )
+}
+
+export default Camera
